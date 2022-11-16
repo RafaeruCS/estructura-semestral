d5625a35896e5c2efd5e8fd1446219eb82224fc1
RafaeruCS <118324615+RafaeruCS@users.noreply.github.com>
2022-11-15 22:14:45 -0500

    Add files via upload

---
+++ Presentacion.java
@@ -0,0 +1,13 @@
+public class Presentacion {
+    static public void HojaDePresentacion() {
+      // Hora de presentacion
+      System.out.println("      UNIVERSIDAD TECNOLÓGICA DE PANAMÁ\n       FACULTAD DE INGENIERÍA DE SISTEMAS COMPUTACIONALES\n      DEPARTAMENTO DE COMPUTACIÓN Y SIMULACIÓN DE SISTEMAS");
+      System.out.println("      LICENCIATURA EN DESARROLLO DE SOFTWARE\n    ESTRUCTURA DE DATOS I \n       PROYECTO FINAL: Estructura de Datos Tipo Pila y Colas");
+      System.out.println("Prof. Doris Cueto             Integrantes:");
+      System.out.println("                              Acosta, Javier ");
+      System.out.println("                              Alarcón, Federico  E-8-183324 ");
+      System.out.println("                              Apellido, Rafael  3-752-1051 ");
+      System.out.println("                              Wen, Enrique    8-991-1691 ");
+      System.out.println("             15 de Noviembre del 2022");
+    }
+  }
